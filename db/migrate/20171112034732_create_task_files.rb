55dd60041d799ad32d3d7ca4bfd00984e2f8d7e3
Yusuke Ebihara <dev.biibiebi@gmail.com>
2017-11-12 13:04:25 +0900

    Create task_files and file_task_attachments

--- db/migrate/20171112034732_create_task_files.rb
+++ db/migrate/20171112034732_create_task_files.rb
@@ -1,7 +1,7 @@
 class CreateTaskFiles < ActiveRecord::Migration[5.1]
   def change
     create_table :task_files do |t|
-
+      t.string :attached_file, null: false
       t.timestamps
     end
   end
