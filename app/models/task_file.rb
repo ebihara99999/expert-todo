b14d911a8b2896ff7580e779bd510697eb495130
Yusuke Ebihara <dev.biibiebi@gmail.com>
2017-11-12 23:10:04 +0900

    :cop: rubocop --auto-correct --only Layout/TrailingWhitespace

--- app/models/task_file.rb
+++ app/models/task_file.rb
@@ -1,8 +1,8 @@
 class TaskFile < ApplicationRecord
   mount_uploader :attached_file, TaskFileUploader
-  
+
   has_many :file_task_attachments
   has_many :tasks, through: :file_task_attachments
-  
+
   validates :attached_file, presence: true
 end
