057babdf17f4ddf2f842b88e1ee8893e92cd5280
Yusuke Ebihara <dev.biibiebi@gmail.com>
2017-12-01 00:01:50 +0900

    Fix typo

--- app/controllers/task_states_controller.rb
+++ app/controllers/task_states_controller.rb
@@ -6,7 +6,7 @@ class TaskStatesController < ApplicationController
     if @task.save
       redirect_to tasks_path, notice: "#{@task.task_name}を終了済みに変更しました"
     else
-      render :tasks_path
+      render tasks_path
     end
   end
 end
