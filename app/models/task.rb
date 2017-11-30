db92f9636ee539ebb884a03e969ee6ba9e0fc7f1
Yusuke Ebihara <dev.biibiebi@gmail.com>
2017-12-01 00:16:00 +0900

    Add `dependent: :destroy`

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,6 @@
 class Task < ApplicationRecord
   belongs_to :user
-  has_many :file_task_attachments
+  has_many :file_task_attachments, dependent: :destroy
   has_many :task_files, through: :file_task_attachments
 
   validates :user_id, presence: true
