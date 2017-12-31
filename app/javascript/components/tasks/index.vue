df529bde1e235a549ac88fcbd3a3c8b429dd122c
Yusuke Ebihara <dev.biibiebi@gmail.com>
2017-12-31 12:23:19 +0900

    Adopt vuetify.js to `tasks/task_ends#create`

--- app/javascript/components/tasks/index.vue
+++ app/javascript/components/tasks/index.vue
@@ -12,12 +12,19 @@
                 class="elevation-1"
         >
             <template slot="items" scope="props">
-                <td class="text-xs-right"><router-link :to="{ name: 'editTaskPath', params: {id: props.item.id}  }">編集</router-link></td>
+                <td class="text-xs-right">
+                    <router-link :to="{ name: 'editTaskPath', params: {id: props.item.id}  }">編集</router-link>
+                </td>
                 <td class="text-xs-right">{{ props.item.task_name }}</td>
                 <td class="text-xs-right">{{ props.item.description }}</td>
                 <td class="text-xs-right">{{ props.item.due_date }}</td>
                 <td class="text-xs-right">{{ props.item.created_at }}</td>
-                <td class="text-xs-right"><router-link :to="{ name: 'taskFilesPath', params: {task_id: props.item.id}  }">ファイル一覧</router-link></td>
+                <td class="text-xs-right">
+                    <router-link :to="{ name: 'taskFilesPath', params: {task_id: props.item.id}  }">ファイル一覧</router-link>
+                </td>
+                <td class="text-xs-right">
+                    <v-btn color="primary" v-on:click.native="makeTaskEnd(props.item.id)">終了済みへ変更</v-btn>
+                </td>
             </template>
         </v-data-table>
     </div>
@@ -37,6 +44,7 @@
           {text: '期限', value: 'due_date', sortable: true},
           {text: '作成日', value: 'created_at', sortable: true},
           {text: 'ファイル一覧', value: ''},
+          {text: 'タスクの終了', value: ''},
         ],
       }
     },
@@ -44,6 +52,31 @@
       this.fetchTasks();
     },
     methods: {
+      makeTaskEnd(task_id) {
+        let config = {
+          headers: {
+            'content-type': 'application/json',
+            'Authorization': localStorage.getItem('auth-token')
+          }
+        };
+
+        let params = {
+          'id': task_id
+        };
+
+        if (process.env.RAILS_ENV != "test") {
+          // テストだとdocument.getElementsByName('csrf-token')[0]が取得できず、エラーが起きる
+          axios.defaults.headers['X-CSRF-TOKEN'] = document.getElementsByName('csrf-token')[0].content;
+        }
+
+        axios.post('/tasks/task_end', params, config).then(() => {
+          this.$router.push('/tasks');
+        }).catch((response) => {
+          console.log(response);
+          this.$router.push('/');
+        });
+      },
+
       fetchTasks() {
         let config = {
           headers: {
