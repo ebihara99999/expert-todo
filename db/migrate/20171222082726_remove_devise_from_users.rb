42362ece1ca5af112a1219ccdb140adb7055226f
Yusuke Ebihara <dev.biibiebi@gmail.com>
2018-02-08 00:26:53 +0900

    :cop: correct Rails/ReversibleMigration

--- db/migrate/20171222082726_remove_devise_from_users.rb
+++ db/migrate/20171222082726_remove_devise_from_users.rb
@@ -1,19 +1,19 @@
 class RemoveDeviseFromUsers < ActiveRecord::Migration[5.1]
   def change
-    remove_column :users, :reset_password_token
-    remove_column :users, :reset_password_sent_at
-    remove_column :users, :remember_created_at
-    remove_column :users, :sign_in_count
-    remove_column :users, :current_sign_in_at
-    remove_column :users, :last_sign_in_at
-    remove_column :users, :current_sign_in_ip
-    remove_column :users, :last_sign_in_ip
-    remove_column :users, :confirmation_token
-    remove_column :users, :confirmed_at
-    remove_column :users, :confirmation_sent_at
-    remove_column :users, :unconfirmed_email
-    remove_column :users, :provider
-    remove_column :users, :uid
-    remove_column :users, :tokens
+    remove_column :users, :reset_password_token, :string
+    remove_column :users, :reset_password_sent_at, :datetime
+    remove_column :users, :remember_created_at, :datetime
+    remove_column :users, :sign_in_count, :integer
+    remove_column :users, :current_sign_in_at, :datetime
+    remove_column :users, :last_sign_in_at, :datetime
+    remove_column :users, :current_sign_in_ip, :inet
+    remove_column :users, :last_sign_in_ip, :inet
+    remove_column :users, :confirmation_token, :string
+    remove_column :users, :confirmed_at, :datetime
+    remove_column :users, :confirmation_sent_at, :datetime
+    remove_column :users, :unconfirmed_email, :string
+    remove_column :users, :provider, :string
+    remove_column :users, :uid, :string
+    remove_column :users, :tokens, :string
   end
 end
