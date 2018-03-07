37d3331277229349e20cd36fdc3d86da1c7716f5
Yusuke Ebihara <dev.biibiebi@gmail.com>
2018-03-08 00:30:23 +0900

    Remove unnecessary entry file

--- app/javascript/packs/todo.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue/dist/vue.esm';
-import taskFileForm from '../components/task_files/new.vue';
-
-new Vue({
-    el: '#app',
-    components: {
-      'task-file-form': taskFileForm,
-    },
-});
